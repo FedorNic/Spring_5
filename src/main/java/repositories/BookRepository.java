fd86fba6bba7fa13e3f44178ebad8f0cf3a78fc3
FedorNic <dmitrievfedor@yandex.ru>
2024-02-05 20:12:37 +0300

    initial commit

---
+++ src/main/java/repositories/BookRepository.java
@@ -0,0 +1,16 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import models.Book;
+
+/**
+ * Класс описывает взаимодействие с хранилищем книг
+ */
+/**
+ * Класс описывает взаимодействие с хранилищем книг
+ */
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
